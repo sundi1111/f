c0372dad4bdad08728d2dc8649ae71a43f7d1dc1
Julie9383186 <dipjuolty883@gmail.com>
2024-06-12 22:58:14 +0800

    jsValueDate

--- assets/Javascript/login.js
+++ assets/Javascript/login.js
@@ -10,6 +10,17 @@ function closeModal(event) {
 window.onclick = closeModal;
 window.ontouchstart = closeModal;
 
+// 日期抓取
+document.addEventListener('DOMContentLoaded', (event) => {
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var dd = String(today.getDate()).padStart(2, '0');
+    var formattedToday = yyyy + '-' + mm + '-' + dd;
+
+    document.getElementById('birthday').value = formattedToday;
+});
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
